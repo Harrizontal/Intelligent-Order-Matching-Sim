ecde95b7f0196975609849ed182b07ec949eb71d
Harrison Wong Jun Yong <Harrisonwjy@hotmail.com>
2020-06-06 11:38:07 +0800

    Remove unused files

--- src/Config.js
+++
@@ -1,8 +0,0 @@
-export const Config = {
-  SERVER_URL: "http://localhost",
-  MAPBOX_API:
-    "pk.eyJ1IjoiaGFycml6b250YWwiLCJhIjoiY2l6YWw3YW90MDQ1NzJ3cDl5bXd4M2Y4aSJ9.CnTz5K2ShZcuLiG0xYLBKw",
-  LONG: 104.06349,
-  LAT: 30.67,
-  ZOOM: 12
-};
